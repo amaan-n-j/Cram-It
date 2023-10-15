8f46c780fb2b8167387e44f2453e8f25002c372c
amaan-nj <amaanjamadar8@gmail.com>
2023-10-15 22:17:57 +0530

    Add Huffman

--- LZW/lzw_main.cpp
+++ LZW/lzw_main.cpp
@@ -5,74 +5,74 @@
 
 using namespace std;
 
+class LZW_CD{
+	public:
+		vector<int> encoder(string text){
+			int dictSize = 256;
+			string str;
+			unordered_map<string , int> dictionary;
+			for(int i = 0 ; i < dictSize; i++){
+				str +=  (char) i;
+				dictionary.insert({str,i});
+				str = "";
+			}
 
-vector<int> encoder(string text){
-	int dictSize = 256;
-	string str;
-	unordered_map<string , int> dictionary;
-	for(int i = 0 ; i < dictSize; i++){
-		str +=  (char) i;
-		dictionary.insert({str,i});
-		str = "";
-	}
+			str = "";
+			vector<int> output ;
 
-	str = "";
-	vector<int> output ;
+			for(char character : text ){
+				string srchStr = str + character;
+				if(dictionary.count(srchStr)){
+					str = srchStr;
+				}
+				else{
+					output.insert(output.end(),dictionary[str]);
+					dictionary.insert({srchStr,dictSize++});
+					str = character;
+				}	
+			}
+			if(!str.empty()){
+				output.insert(output.end(),dictionary[str]);
+			}
 
-	for(char character : text ){
-		string srchStr = str + character;
-		if(dictionary.count(srchStr)){
-			str = srchStr;
+			return output;
+
 		}
-		else{
-			output.insert(output.end(),dictionary[str]);
-			dictionary.insert({srchStr,dictSize++});
-			str = character;
-		}	
-	}
-	if(!str.empty()){
-			output.insert(output.end(),dictionary[str]);
-	}
 
-	return output;
+		string decoder(vector<int> output){
+			int dictSize = 256;
+			string str;
+			unordered_map<int, string > dictionary;
+			for(int i = 0 ; i < dictSize; i++){
+				str += (char) i;
+				dictionary.insert({i,str});
+				str  = "";
+			}
 
-}
+			str = "";
+			string characters ;
+			characters += (char) output.at(0);
+			output.erase(output.begin());
+			string finalStr;
+			finalStr += characters;
 
-string decoder(vector<int> output){
-	int dictSize = 256;
-	string str;
-	unordered_map<int, string > dictionary;
-	for(int i = 0 ; i < dictSize; i++){
-		str += (char) i;
-		dictionary.insert({i,str});
-		str  = "";
-	}
-	
-	str = "";
-	string characters ;
-      	characters += (char) output.at(0);
-	output.erase(output.begin());
-	string finalStr;
-	finalStr += characters;
-	
-	for(int code : output ){
-		string strValue = dictionary.count(code) ? dictionary[code] : characters + characters[0];
-		finalStr += strValue;
-		dictionary.insert({dictSize++,characters + strValue[0]});
-		characters = strValue;
-	}
-	return finalStr;	
-}
+			for(int code : output ){
+				string strValue = dictionary.count(code) ? dictionary[code] : characters + characters[0];
+				finalStr += strValue;
+				dictionary.insert({dictSize++,characters + strValue[0]});
+				characters = strValue;
+			}
+			return finalStr;	
+		}
+};
 
 int main(){
-	string str1 = "WYS*WYGWYS*WYSWYSG";
-	vector<int> output = encoder(str1);
-	for(int j : output){
-		cout << j <<'\n';
-	}
-	cout << "-----------------------------\n";
-        string decodedStr = decoder(output);
-	cout<< decodedStr << " --\t-- " << str1;       
-	return 0;
+	LZW_CD compressor;
+	string str1 = "Amaan Nayeem Jamdar";
+	vector<int> output = compressor.encoder(str1);
+	string str2 = compressor.decoder(output);
+	cout << str1<<"\n";
+	cout << str2;
+		return 0;
 }
 
